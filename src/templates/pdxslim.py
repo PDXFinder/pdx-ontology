20f0cd0970a6749706e447fe432b0cd47619a61b
Zoe May Pendlington <zmp@ebi.ac.uk>
2018-03-27 16:54:32 +0100

    updated pdxslim.py

--- src/templates/pdxslim.py
+++ src/templates/pdxslim.py
@@ -5,12 +5,12 @@ with open('pdx-cancer.tsv', 'rb') as f:
 
 def extract(x):
     global lst
-    global slim
-    slim = ('../ontology/results/slim.txt')
+    global xrefs
+    xrefs = ('../ontology/results/ncit-xrefs.txt')
     lst = [item[x] for item in l] #takes the first value of each nested list (IDs)
     lst = lst[2:] #removes the two rows of "titles"
     lst = [y for y in lst if "PDXO:" not in y] #removes any PDXO: xrefs
-    with open (slim, "w") as output: #writes the result to the slim.txt file
+    with open (xrefs, "w") as output: #writes the result to the slim.txt file
         writer = csv.writer(output, lineterminator='\n')
         for xref in lst:
             writer.writerow([xref])
